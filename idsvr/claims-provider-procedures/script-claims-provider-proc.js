ac165d77515094898b047d1c0e0b85dd960c2240
Judith Kahrer <judith.kahrer@curity.io>
2026-03-16 10:34:06 +0100

    Update configuration of the authorization server to support ephemeral clients

--- idsvr/claims-provider-procedures/script-claims-provider-proc.js
+++ idsvr/claims-provider-procedures/script-claims-provider-proc.js
@@ -8,14 +8,23 @@ function result(context) {
   var dataSourceAttributes = dataSource.getAttributes(context.subjectAttributes.subject);
   var account = dataSourceAttributes.getRow(0);
   var accountAttributes = JSON.parse(account.attributes);
+  var clientType = context.client.properties.client_type;
+  var clientAssuranceLevel = context.client.properties.client_assurance_level;
+
   if (!accountAttributes) {
     return {};
+  }
+
+  if (context.client.id.startsWith("https://")) {
+    logger.warn("Preparing claims for ephemeral client.")
+    clientType = clientType || "mcp";
+    clientAssuranceLevel = clientAssuranceLevel || 1;
   }
 
   // Also include client properties
   return { 
     region: accountAttributes.region || '',
-    client_type: context.client.properties.client_type || '',
-    client_assurance_level: context.client.properties.client_assurance_level || 0,
+    client_type: clientType || '',
+    client_assurance_level: clientAssuranceLevel || 0,
   }
 }
